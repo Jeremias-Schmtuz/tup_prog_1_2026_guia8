d3659890dfdee63c33df508bd172ed1f74f6ef3d
Jeremias-Schmutz <schmutzjeremias@gmail.com>
2026-05-23 16:54:19 -0600

    Guia 8 en proceso

---
+++ Guia 8/Ejercicio 3/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    internal class Program
+    {
+        #region Variables "LOCALES"
+        static string nombre1;
+        static string nombre2;
+        static string nombre3;
+        static int numeroLibreta1;
+        static int numeroLibreta2;
+        static int numeroLibreta3;
+        static int orden;
+        #endregion
+
+        #region REGISTRAR NOMBRES Y NUMEROS DE LIBRETAS
+        static void RegistrarNombresYNumerosDeLibretas()
+        {
+            nombre1 = Program.MostrarPantallaSolicitarAlumno();
+            numeroLibreta1 = Program.SolicitarNumeroDeLibretas();
+            nombre2 = Program.MostrarPantallaSolicitarAlumno();
+            numeroLibreta2 = Program.SolicitarNumeroDeLibretas();
+            nombre3 = Program.MostrarPantallaSolicitarAlumno();
+            numeroLibreta3 = Program.SolicitarNumeroDeLibretas();
+        }
+        #endregion
+
+        #region SOLICITAR ALUMNNO
+        static string MostrarPantallaSolicitarAlumno()
+        {
+            string nombre;
+            Console.WriteLine("Ingrese nombre del alumno");
+            nombre = Console.ReadLine();
+            return nombre;
+        }
+        #endregion
+
+        #region SOLICITAR NUMERO DE LIBRETA
+        static int SolicitarNumeroDeLibretas()
+        {
+            int numeroLibreta;
+            Console.WriteLine("Ingrese numero de libreta del alumno");
+            numeroLibreta = Convert.ToInt32(Console.ReadLine());
+            return numeroLibreta;
+        }
+        #endregion
+
+        #region MOSTRAR PANTALLA Y MOSTRAR LISTA ORDENADA
+        static void MostrarPantallaMostrarListaOrdenada()
+        {
+            if (numeroLibreta1 > numeroLibreta2 && numeroLibreta1 > numeroLibreta3)
+            {
+                if (numeroLibreta2 > numeroLibreta3)
+                {
+                    Console.WriteLine(nombre1 + " / " + numeroLibreta1 + "\n" + nombre2 + " / " + numeroLibreta2 + "\n" + nombre3 + " / " + numeroLibreta3);
+                }
+                else
+                {
+                    Console.WriteLine(nombre1 + " / " + numeroLibreta1 + "\n" + nombre3 + " / " + numeroLibreta3 + "\n" + nombre2 + " / " + numeroLibreta2);
+                }
+            }
+            else if (numeroLibreta2 > numeroLibreta1 && numeroLibreta2 > numeroLibreta3)
+            {
+                if (numeroLibreta1 > numeroLibreta3)
+                {
+                    Console.WriteLine(nombre2 + " / " + numeroLibreta2 + "\n" + nombre1 + " / " + numeroLibreta1 + "\n" + nombre3 + " / " + numeroLibreta3);
+                }
+                else
+                {
+                    Console.WriteLine(nombre2 + " / " + numeroLibreta2 + "\n" + nombre3 + " / " + numeroLibreta3 + "\n" + nombre1 + " / " + numeroLibreta1);
+                }
+            }
+            else if (numeroLibreta3 > numeroLibreta1 && numeroLibreta3 > numeroLibreta2)
+            {
+                if (numeroLibreta1 > numeroLibreta2)
+                {
+                    Console.WriteLine(nombre3 + " / " + numeroLibreta3 + "\n" + nombre1 + " / " + numeroLibreta1 + "\n" + nombre2 + " / " + numeroLibreta2);
+                }
+                else
+                {
+                    Console.WriteLine(nombre3 + " / " + numeroLibreta3 + "\n" + nombre2 + " / " + numeroLibreta2 + "\n" + nombre1 + " / " + numeroLibreta1);
+                }
+            }
+        }
+        #endregion
+
+        #region SOLICITAR OPCION
+        static int MostrarPantallaSolicitarOpcion()
+        {
+            int opcion;
+            Console.WriteLine("1-Ingresar las notas de los 3 alumnos + numero de libretas");
+            Console.WriteLine("2-Mostrar lista ordenada");
+            Console.WriteLine("3-Salir");
+            opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            int opcion;
+            do
+            {
+                opcion = MostrarPantallaSolicitarOpcion();
+                switch (opcion)
+                {
+                    case 1: Program.RegistrarNombresYNumerosDeLibretas(); break;
+                    case 2: Program.MostrarPantallaMostrarListaOrdenada(); break;
+                    default:
+                        Console.WriteLine("Opcion invalida");
+                        Console.WriteLine("Redirigiendo al menu de opciones...");
+                        Thread.Sleep(2500); break;
+                }
+            }
+            while (opcion >= 1 && opcion <= 2);
+        }
+    }
+}
---
+++ Guia 8/Ejercicio 4/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4
+{
+    internal class Program
+    {
+
+        #region Variables "LOCALES"
+        static string jugador1;
+        static string jugador2;
+        static int setGanados1;
+        static int setGanados2;
+        #endregion
+
+        #region REGISTRAR JUGADORES
+        static void RegistrarJugadores(string nombre1, string nombre2)
+        {
+            jugador1 = nombre1;
+            jugador2 = nombre2;
+        }
+        #endregion
+
+        #region REGISTRAR RESULTADOS SETS
+        static void RegistrarResultadosSets(int resultado1, int resultado2)
+        {
+            if (resultado1 > resultado2)
+            {
+                setGanados1++;
+            }
+            else
+            {
+                setGanados2++;
+            }
+        }
+        #endregion
+
+        #region DETERMINAR GANADOR
+        static string DeterminarGanador()
+        {
+            if (setGanados1 > setGanados2)
+            {
+                return jugador1;
+            }
+            else
+            {
+                return jugador2;
+            }
+        }
+        #endregion
+
+        #region SOLICITAR OPCION MENU
+        static int MostrarPantallaSolicitarOpcionMenu()
+        {
+            Console.WriteLine("1-Ingresar los nombres de los juegadores");
+            Console.WriteLine("2-Ingresar los resultados de cada set de los jugadores");
+            Console.WriteLine("3-Mostrar el ganador");
+            int opcion = Convert.ToInt32(Console.ReadLine());
+            return opcion;
+        }
+        #endregion
+
+        #region SOLICITAR NOMBRE DE LOS JUGADORES
+        static void MostrarPantallaSolicitarNombreJugadores()
+        {
+            Console.WriteLine("-REGISTRO DE JUGADORES-");
+            Console.WriteLine("Ingrese nombre de jugador 1");
+            string nombre1 = Console.ReadLine();
+            Console.WriteLine("Ingrese nombre de jugdaor 2");
+            string nombre2 = Console.ReadLine();
+            RegistrarJugadores(nombre1, nombre2);
+        }
+        #endregion
+
+        #region SOLICITAR RESULTADOS
+        static void MostrarPantallaSolicitarResultados()
+        {
+            for(int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"-REGISTRAR RESULTADOS SET {i}-");
+                Console.Write("Ingrese el resultado del jugador 1:");
+                int resultado1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ingrese el resultado del jugador 2:");
+                int resultado2 = Convert.ToInt32(Console.ReadLine());
+                RegistrarResultadosSets(resultado1, resultado2);
+            }
+        }
+        #endregion
+
+        #region MOSTRAR GANADOR
+        static void MostrarPantallaGanador()
+        {
+            Console.WriteLine("El ganador es: " + DeterminarGanador());
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            int opcion;
+            do
+            {
+                opcion = MostrarPantallaSolicitarOpcionMenu();
+                switch (opcion)
+                {
+                    case 1: MostrarPantallaSolicitarNombreJugadores(); break;
+                    case 2: MostrarPantallaSolicitarResultados(); break;
+                    case 3: MostrarPantallaGanador(); break;
+                    default:
+                        Console.WriteLine("Opcion invalida.");
+                        Console.WriteLine("Redirigiendo al menu de opciones...");
+                        Thread.Sleep(2500);
+                        break;
+                }
+            }
+            while (opcion != 3);
+        }
+    }
+}
